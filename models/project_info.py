741921b6a574d22a1b06f583fbeaa3984237bdff
Abhishek <jawanpuriaabhishek@gmail.com>
2026-07-05 21:00:56 +0530

    created classes for project info and individual applications info

---
+++ models/project_info.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass,field
+from pathlib import Path
+@dataclass
+class ApplicationInfo:
+ application_path: Path=None
+ language: str = "Unknown"
+ framework: str= "Unknown"
+ package_manager: str="Unknown"
+ databse: str="Unknown"
+ has_dockerfile: bool=False
+ 
+
+@dataclass
+class ProjectInfo:
+    project_name: str = ""
+    apps_info:list[ApplicationInfo]=field(default_factory=list)
+    has_compose: bool = False
+    has_github_workflow: bool = False
