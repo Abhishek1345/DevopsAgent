13698558da7ffb3c7a6776ac29ed2d46bd92049e
Abhishek <jawanpuriaabhishek@gmail.com>
2026-07-12 16:15:20 +0530

    created agent to generate .dockerignore files

--- graph/Graph.py
+++ graph/Graph.py
@@ -2,6 +2,7 @@ from langgraph.graph import StateGraph,START,END
 from graph.state import State
 from graph.writer import write
 from agents.DockerAgent import docker_agent
+from agents.DockerIgnoreAgent import docker_ignore_agent
 def docker_complete(state:State)->State:
     if state["application_index"]>=len(state["project_info"].apps_info):
         return "continue"
@@ -9,10 +10,12 @@ def docker_complete(state:State)->State:
         return "loop"
 graph=StateGraph(State)
 graph.add_node("docker_agent",docker_agent)
+graph.add_node("docker_ignore_agent",docker_ignore_agent)
 graph.add_node("writer",write)
 graph.add_edge(START,"docker_agent")
+graph.add_edge("docker_agent","docker_ignore_agent")
 graph.add_conditional_edges(
-    "docker_agent",
+    "docker_ignore_agent",
     docker_complete,
     {
     "loop":"docker_agent",
